2d25464bb68cb60ea093ab8b4959d6f3fd5a3434
Joel Dowdy <112461088+DowdyJ@users.noreply.github.com>
2024-03-31 19:21:43 -0700

    Update run_container.bat

--- run_container.bat
+++ run_container.bat
@@ -1,1 +1,3 @@
-docker run --restart=unless-stopped --cap-add=SYS_ADMIN -v ./data/:/app/bot_voter/data/ -it dowdyj/botvoter-backend --name="bot-voter"
+docker stop bot-voter
+docker rm bot-voter
+docker run --restart=unless-stopped --cap-add=SYS_ADMIN -v ./data/:/app/bot_voter/data/ -it --name="bot-voter" dowdyj/botvoter-backend
